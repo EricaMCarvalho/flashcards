0b85291b19c742a0c82a67e90067ce9b79337077
Erica Carvalho <ericamcar28@gmail.com>
2021-04-14 23:17:02 +0100

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('auth');
+});
+
+router.get('/google/callback', (req, res) => {
+  res.send('auth');
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const MongoStore = require('connect-mongo')(session);
+const connectDB = require('./config/database');
+
+const app = express();
+
+// Load config
+require('dotenv').config({ path: './config/.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+// Connect to MongoDB
+connectDB();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
